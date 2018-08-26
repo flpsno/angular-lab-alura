742caf9a6a62c15d6c2a37a9f707ad5aa5de56f7
Felipe Santos <flpsno@hotmail.com>
2018-08-26 07:18:28 -0300

    adicionado o componente de login

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { SignInComponent } from "./sigin/signin.component";
+
+
+@NgModule({
+    declarations: [ SignInComponent ]
+})
+export class HomeModule {}
