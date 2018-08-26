722d41ae78c29e5e8477cd85481f18eb48323fae
Felipe Santos <flpsno@hotmail.com>
2018-08-26 06:32:17 -0300

    Adicionada Output property

--- src/app/photos/photo-list/search/search.component.ts
+++ src/app/photos/photo-list/search/search.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy, Output, EventEmitter, Input } from "@angular/core";
 import { Subject } from "rxjs";
-import { debounceTime } from "rxjs/operators";
+import { debounceTime, filter } from "rxjs/operators";
 
 @Component({
     selector: 'app-search',
@@ -8,12 +8,15 @@ import { debounceTime } from "rxjs/operators";
 })
 export class SearchComonent implements OnInit, OnDestroy {
 
+    @Output() onTyping: EventEmitter<string> = new EventEmitter<string>();
+    @Input() value: string = '';
     debounce: Subject<string> = new Subject<string>();
 
     ngOnInit(): void {
 
         this.debounce
             .pipe(debounceTime(300))
+            .subscribe(filter => this.onTyping.emit(filter));
     }
 
     ngOnDestroy(): void {
