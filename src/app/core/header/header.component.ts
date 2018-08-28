681bb755111518e0847f00655c682159e2f8f4ba
Felipe Santos <flpsno@hotmail.com>
2018-08-27 22:42:32 -0300

    adicionado metodo de logout e classe auth guard

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -11,7 +12,15 @@ export class HeaderComponent {
 
     user$: Observable<User>;
 
-    constructor(private userService: UserService){
+    constructor(
+        private userService: UserService,
+        private router: Router
+    ){
         this.user$ = userService.getUser();
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 }
