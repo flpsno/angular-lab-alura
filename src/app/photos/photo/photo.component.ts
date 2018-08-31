88bb3440a0013b03282e0f6d636fc97bb0ab4215
Felipe Santos <flpsno@hotmail.com>
2018-08-30 22:58:23 -0300

    adequando a exibição das fotos

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,11 +1,25 @@
 import { Component, Input } from '@angular/core';
 
+const cloud = 'http://localhost:3000/imgs/';
 @Component({
     selector: 'app-photo',
     templateUrl: 'photo.component.html'
 })
 
 export class PhotoComponent {
+
+    private _url = '';
     @Input() description = '';
-    @Input() url = '';
+    @Input() set url(url: string) {
+
+        if(url && url.startsWith('data')) {
+            this._url = url;
+        } else {
+            this._url = cloud + url;
+        }
+    };
+
+    get url() {
+        return this._url;
+    }
 }
