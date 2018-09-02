eef0439d3f4ad5587370fa88e777d811d63cd729
Felipe Santos <flpsno@hotmail.com>
2018-09-01 22:22:01 -0300

    adição de foto selecionado da api

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
+import { Observable } from 'rxjs';
 
 @Component({
-    templateUrl: './photo-details.component.html'
+    templateUrl: './photo-details.component.html',
+    styleUrls: ['./photo-details.component.css']
 })
 export class PhotoDetailsComponent implements OnInit {
 
-    photo: Photo;
+    photo$: Observable<Photo>;
 
     constructor(
         private route: ActivatedRoute,
@@ -17,9 +19,7 @@ export class PhotoDetailsComponent implements OnInit {
 
     ngOnInit(): void {
 
-        const id = this.route.snapshot.params.photoId;
-        this.photoService
-            .findById(id)
-            .subscribe(photo => this.photo = photo);
+        this.photo$ = this.photoService
+            .findById(this.route.snapshot.params.photoId);
     }
 }
