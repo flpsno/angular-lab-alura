ef4a277d5f5f82b3b06effb30a116ba34f347fe7
Felipe Santos <flpsno@hotmail.com>
2018-09-03 21:46:20 -0300

    adicionado serviço de title

--- src/app/home/sigin/signin.component.ts
+++ src/app/home/sigin/signin.component.ts
@@ -3,6 +3,9 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { AuthService } from "../../core/auth/auth.service";
 import { Router } from "@angular/router";
 import { PlatformDetectorService } from "../../core/platform-detector/platform-detector.service";
+import { Title } from "@angular/platform-browser";
+import { Title } from "@angular/platform-browser";
+import { Title } from "@angular/platform-browser";
 
 @Component({
     templateUrl: './signin.component.html'
@@ -16,11 +19,13 @@ export class SignInComponent implements OnInit {
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router,
-        private platformDetectorService: PlatformDetectorService
+        private platformDetectorService: PlatformDetectorService,
+        private titleService: Title
     ) { }
 
     ngOnInit(): void {
 
+        this.titleService.setTitle('Login');
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
