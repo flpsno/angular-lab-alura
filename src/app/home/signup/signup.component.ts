7545a0e03a70c059acedc70ea96921db424d0002
Felipe Santos <flpsno@hotmail.com>
2018-09-07 07:17:02 -0300

    adicionada validação crossfield

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -6,6 +6,7 @@ import { NewUser } from './new-user';
 import { SignUpService } from './signup.service';
 import { Router } from '@angular/router';
 import { PlatformDetectorService } from '../../core/platform-detector/platform-detector.service';
+import { userNamePassword } from './username-password.validator';
 
 
 @Component({
@@ -52,6 +53,8 @@ export class SignUpComponent implements OnInit {
                 Validators.required,
                 Validators.minLength(8),
                 Validators.maxLength(14)]]
+        }, {
+            validator: userNamePassword 
         });
 
         this.platformDetectorService.isPlatformBrowser &&
